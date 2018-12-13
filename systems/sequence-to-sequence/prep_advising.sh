3e11066e19dfbc9203d137a1c02de16a63cfacd6
Ankur Garg <ankurgarg101@gmail.com>
2018-12-12 23:18:55 -0600

    fixes for conversion to python3
    Signed-off-by: Ankur Garg <ankurgarg101@gmail.com>

--- systems/sequence-to-sequence/prep_advising.sh
+++ systems/sequence-to-sequence/prep_advising.sh
@@ -6,11 +6,11 @@ export WORK=$S2S_HOME/work
 cd ../../
 export TEXT2SQL_HOME=$(pwd)
 cd $TEXT2SQL_HOME/tools
-python2 new_format_processor.py $TEXT2SQL_HOME/data/advising.json $WORK/advising_data/
+python3 new_format_processor.py $TEXT2SQL_HOME/data/advising.json $WORK/advising_data/
 
 cd $S2S_HOME
-python2 make_schema_loc_file.py $WORK/advising_data/query_split/ $TEXT2SQL_HOME/data/advising-schema.csv
-python2 make_schema_loc_file.py $WORK/advising_data/question_split/ $TEXT2SQL_HOME/data/advising-schema.csv
+python3 make_schema_loc_file.py $WORK/advising_data/query_split/ $TEXT2SQL_HOME/data/advising-schema.csv
+python3 make_schema_loc_file.py $WORK/advising_data/question_split/ $TEXT2SQL_HOME/data/advising-schema.csv
 cd bin/tools
-python2 generate_copying_vocab.py $WORK/advising_data/query_split/
-python2 generate_copying_vocab.py $WORK/advising_data/question_split/
+python3 generate_copying_vocab.py $WORK/advising_data/query_split/
+python3 generate_copying_vocab.py $WORK/advising_data/question_split/
